6f34371f0ac8291123e7a55273a7d7dd51875006
이혜성 <main@hyeseong.local>
2022-08-29 18:18:40 +0900

    database connection with docker-compose

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable({})
+export class AuthService {
+  signup() {
+    return { msg: ' I have signup' };
+  }
+
+  signin() {
+    return { msg: ' I have signin' };
+  }
+}
