e70c434e0f451e5d50e549c61d3b967cc254aa5e
이혜성 <main@hyeseong.local>
2022-08-29 23:54:12 +0900

    nestjs global pipe

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, Req } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto';
 
@@ -8,7 +8,6 @@ export class AuthController {
 
   @Post('signup')
   signup(@Body() dto: AuthDto) {
-    console.log(dto);
     return this.authService.signup();
   }
 
