3e7c4c108839bd85abe5893a6d7a5b0f42ec9794
이혜성 <main@hyeseong.local>
2022-08-31 04:15:46 +0900

    prisma database tearing down logic

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -13,4 +13,7 @@ export class PrismaService extends PrismaClient {
       },
     });
   }
+  cleanDb() {
+    return this.$transaction([this.bookmark.deleteMany(), this.user.deleteMany()]);
+  }
 }
