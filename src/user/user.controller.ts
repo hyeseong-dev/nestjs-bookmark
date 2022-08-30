8d9b0eb5d54dd96f3d455a32eaf7562d876b04be
이혜성 <main@hyeseong.local>
2022-08-31 00:11:02 +0900

    custom decorator 1. get specific property from user instance

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,17 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Controller, Get, Patch, Req, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { User } from '@prisma/client';
 import { Request } from 'express';
+import { GetUser } from '../auth/decorator';
 import { JwtGuard } from '../auth/guard/jwt.guard';
 
 @Controller('users')
 export class UserController {
   @UseGuards(JwtGuard)
   @Get('me')
-  getMe(@Req() req: Request) {
-    return req.user;
+  getMe(@GetUser() user: User, @GetUser('email') email: string) {
+    console.log(email);
+    console.log(user);
+    return user;
   }
 }
